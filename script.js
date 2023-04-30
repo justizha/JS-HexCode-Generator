7fd47fce595982fa768cf756e8f937877451956d
Alfath Izha Barikallah <alfathizhaaaaaaa@gmail.com>
2023-04-30 20:24:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+  const myColor = () => {
+        const color = document.getElementById('color').value;
+        document.body.style.backgroundColor = color;
+        document.getElementById("box").value = color;
+    }
+
+    const Copy = () => {
+        const copyText = document.getElementById("box");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyText.value)
+    }
+
+
+
+    document.getElementById('color').addEventListener('input', myColor);
